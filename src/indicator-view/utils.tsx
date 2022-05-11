6208ab6c869b75d16abc52da6baf1f31a9bc73e2
jdev-LRHR <84962016+jdev-LRHR@users.noreply.github.com>
2022-05-11 22:05:14 +0900

    Update : axios base url 설정

--- src/indicator-view/utils.tsx
+++ src/indicator-view/utils.tsx
@@ -5,9 +5,9 @@ export const getIndicatorsStartToEndDate = (startingDate: Date, endingDate: Date
   const startDate = moment(startingDate).format('YYYY-MM-DD')
   const endDate = moment(endingDate).format('YYYY-MM-DD')
   console.log('indicators')
-  return axios.get(`http://localhost:8000/api/indicators/?start_date=${startDate}&end_date=${endDate}`)
+  return axios.get(`/api/indicators/?start_date=${startDate}&end_date=${endDate}`)
 }
 
 export const getWeeklyIndicators = (year: number, month: number) => {
-  return axios.get(`http://localhost:8000/api/indicators/?year=${year}&month=${month}&weekly_aggregate=${true}`)
+  return axios.get(`/api/indicators/?year=${year}&month=${month}&weekly_aggregate=${true}`)
 }
